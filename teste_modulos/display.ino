84d359b418fd392dc5fbe3704877f2b1e92bbc68
Paulo Prates <paulo.prates01@hotmail.com>
2024-08-16 13:06:03 -0300

    Programaçã das telas estáticas e início dos testes

---
+++ teste_modulos/display.ino
@@ -0,0 +1,155 @@
+// Função para centralizar o texto
+int centralizarDisplay(String texto) {
+  int posicao = (COLUNAS_DISPLAY - texto.length()) / 2;
+
+  if (posicao < 0) {
+    return 0;
+  } else {
+    return posicao;
+  }
+}
+
+/* Telas */
+// Tela Incial
+int telaInicial() {
+  display.clear();
+
+  String titulo = "Bobinadeira V1.0";
+
+  display.setCursor(centralizarDisplay(titulo), 0);
+  display.print(titulo);
+  display.setCursor(0, 2);
+  display.print("1 - Bobinar");
+  display.setCursor(0, 3);
+  display.print("2 - Recalibrar");
+
+  return 1;
+}
+
+/* Telas de execução */
+// Tela de inicialização do processo
+int telaBobinar() {
+  display.clear();
+
+  String titulo = "Bobinar";
+
+  display.setCursor(centralizarDisplay(titulo), 0);
+  display.print(titulo);
+  display.setCursor(0, 1);
+  display.print("1 - Novo Indutor");
+  display.setCursor(0, 2);
+  display.print("2 - Repetir Indutor");
+  display.setCursor(0, 3);
+  display.print("0 - Voltar");
+
+  return 20;
+}
+
+// Tela de dimensionamento do indutor
+int telaIndutor() {
+  display.clear();
+
+  String titulo = "Bobinar";
+
+  display.setCursor(centralizarDisplay(titulo), 0);
+  display.print(titulo);
+  display.setCursor(0, 1);
+  display.print("Espiras: ");
+  display.setCursor(0, 2);
+  display.print("Comprimento: ");
+  display.setCursor(0, 3);
+  display.print("Diametro (fio): ");
+}
+
+// Tela de monitoramento do processo
+int telaProgresso(int espiras, int camadas) {
+  display.clear();
+
+  String titulo = "Andamento";
+  String linha1 = "Espira: " + String(espiras);
+  String linha2 = "Camada: " + String(camadas);
+
+  display.setCursor(centralizarDisplay(titulo), 0);
+  display.print(titulo);
+  display.setCursor(0, 2);
+  display.print(linha1);
+  display.setCursor(0, 3);
+  display.print(linha2);
+
+  return 21;
+}
+
+/* Telas de calibragem */
+// Tela de calibragem do RPM
+int calibrarRPM(int tempo) {
+  display.clear();
+
+  String linha1 = "Calibrando RPM";
+  String linha2 = "Aguarde " + String(tempo) + "s";
+
+  display.setCursor(centralizarDisplay(linha1), 1);
+  display.print(linha1);
+  display.setCursor(centralizarDisplay(linha2), 2);
+  display.print(linha2);
+
+  return 31;
+}
+
+// Tela de exibição do RPM
+int confirmarRPM(int rpm) {
+  display.clear();
+
+  String linha1 = "RPM Medido:";
+  String linha2 = String(rpm) + " RPM";
+
+  display.setCursor(centralizarDisplay(linha1), 1);
+  display.print(linha1);
+  display.setCursor(centralizarDisplay(linha2), 2);
+  display.print(linha2);
+  
+  return 32;
+}
+
+// Tela de calibragem do motor de passo
+int calibrarPasso() {
+  display.clear();
+
+  String linha1 = "Alinhando eixo";
+  String linha2 = "Aguarde...";
+
+  display.setCursor(centralizarDisplay(linha1), 1);
+  display.print(linha1);
+  display.setCursor(centralizarDisplay(linha2), 2);
+  display.print(linha2);
+
+  return 33;
+}
+
+// Tela de conclusão da calibragem
+int concluirCalibragem() {
+  display.clear();
+
+  String linha1 = "Eixo alinhado";
+  String linha2 = "Calibragem concluida";
+
+  display.setCursor(centralizarDisplay(linha1), 1);
+  display.print(linha1);
+  display.setCursor(centralizarDisplay(linha2), 2);
+  display.print(linha2);
+
+  return 30;
+}
+
+// Tela de erro
+int selecaoInvalida() {
+  display.clear();
+  String linha1 = "Comando Invalido!";
+  String linha2 = "Tente Novamente!";
+
+  display.setCursor(centralizarDisplay(linha1), 1);
+  display.print(linha1);
+  display.setCursor(centralizarDisplay(linha2), 2);
+  display.print(linha2);
+
+  return -1;
+}
---
+++ teste_modulos/teste_modulos.ino
@@ -0,0 +1,85 @@
+/* Bibliotecas */
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+
+/* Definições */
+// Display
+#define ENDERECO_DISPLAY 0x38
+#define COLUNAS_DISPLAY 20
+#define LINHAS_DISPLAY 4
+
+// Keypad
+#define LINHAS_TECLADO 4
+#define COLUNAS_TECLADO 4
+
+/* Variáveis */
+// Display
+/* 
+  | Controle de telas
+  | -----------------------------
+  | id -1 = erro de comando
+  | id 1 = Tela de Seleção
+  | id 2x = Telas de execução
+  | id 3x = Telas de Calibragem
+*/
+int telaAtual = 0;
+
+/* 
+  | Instanciamento do display
+  | -------------------------------------
+  | endereço 0x27 no modelo físico
+  | endereço 0x38 para teste no proteus
+*/
+LiquidCrystal_I2C display(0x27, COLUNAS_DISPLAY, LINHAS_DISPLAY);
+
+// Keypad
+byte pinLinhasTeclado[LINHAS_TECLADO] = {2, 3, 4, 5};
+byte pinColunasTeclado[COLUNAS_TECLADO] = {6, 7, 8, 9};
+
+char teclas[LINHAS_TECLADO][COLUNAS_TECLADO] = {
+  '1', '2', '3', 'A',
+  '4', '5', '6', 'B',
+  '7', '8', '9', 'C',
+  '*', '0', '#', 'D'
+};
+
+// Instanciamento do keypad
+Keypad teclado = Keypad(makeKeymap(teclas), pinLinhasTeclado, pinColunasTeclado, LINHAS_TECLADO, COLUNAS_TECLADO);
+
+void setup() {
+  // Inicialização do display
+  display.begin();
+  display.backlight();
+
+  telaAtual = telaInicial();
+}
+
+void loop() {
+  char tecla = teclado.getKey();
+
+  switch (telaAtual) {
+    case 1:
+      switch (tecla) {
+      case '1':
+        telaAtual = telaBobinar();
+        break;
+      case '2':
+        telaAtual = calibrarRPM(5);
+        break;
+    }
+    break;
+    case 20:
+      switch (tecla) {
+        case '1':
+          telaAtual = telaIndutor();
+          break;
+        case '2':
+          telaAtual = telaProgresso(0, 0);
+          break;
+        case '0':
+          telaAtual = telaInicial();
+          break;
+      }
+      break;
+  }
+}
